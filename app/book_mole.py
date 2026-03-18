4527397c49bd61e3a97acbe69a74dd0f6dbaccc6
Aleksandra Leszczyk <aleszczyk.login@gmail.com>
2026-03-18 13:28:35 +0100

    agent rag retrieval

---
+++ app/book_mole.py
@@ -0,0 +1,65 @@
+import os
+import logging
+
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.tools import tool
+from langchain.agents import create_agent
+from langchain.agents.middleware import wrap_model_call, ModelRequest, ModelResponse
+from langchain_openai import ChatOpenAI
+import os
+from langchain_openai import ChatOpenAI
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_core.messages import SystemMessage, HumanMessage, convert_to_messages
+from langchain_core.documents import Document
+
+from dotenv import load_dotenv
+
+
+load_dotenv(override=True)
+
+PROMPT_BOOK_MOLE = """
+You are a knowledgeable scientist specialized in quantum chemistry with access to the datastore.
+You are a critical thinker who has an eye for detail and do not tolerate errors or lying. If you are not sure, you don't answer.
+You answer questions related to quantum chemistry based on the sources.
+"""
+
+MODEL = os.environ["MODEL_KNOWLEDGE_SUMMARY"]
+DB_NAME = os.environ["DB_NAME"]
+
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+vectordb = Chroma(persist_directory=DB_NAME, embedding_function=embeddings)
+retriever = vectordb.as_retriever()
+model_book_mole = ChatOpenAI(temperature=0, model_name=MODEL)
+
+@tool
+def search(query: str) -> str:
+    """Search for information."""
+    return format_docs(retriever.invoke(query, k=10))
+
+book_mole = create_agent(
+    model_book_mole,
+    tools=[search],
+    system_prompt=SystemMessage(
+        content=[
+            {
+                "type": "text",
+                "text": PROMPT_BOOK_MOLE,
+            }
+        ]
+    ),
+)
+
+def format_docs(context: list[str | Document]):
+    result = "<h2 style='color: #ff7800;'>Relevant Context</h2>\n\n"
+    for doc in context:
+        result += (
+            f"<span style='color: #ff7800;'>Source: {doc.metadata['source']}</span>\n\n"
+        )
+        result += doc.page_content + "\n\n"
+    return result
+
+
+
